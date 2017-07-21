de68570a72c3e12c80761e4e9dd4daddd7476092
William Woodruff <william@tuffbizz.com>
2017-07-20 23:47:26 -0400

    inih: 0.1.0

--- lib/inih.rb
+++ lib/inih.rb
@@ -5,7 +5,7 @@ require_relative "../ext/inih/inih"
 # The primary namespace for {INIH}.
 module INIH
   # The current version of ruby-inih.
-  VERSION = "0.0.3"
+  VERSION = "0.1.0"
 
   # Normalize a parsed INI file's values.
   # @api private
